4ae7b8c69b5b21f15f9dd5a409b8ef9ff01d51c3
grayzcale <00xima16@gmail.com>
2025-04-08 05:33:25 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html', t="Home")
+
+@app.route('/about')
+def about():
+	return 'About Page'
+
+# Run the application
+if __name__ == '__main__':
+	app.run(debug=True)
