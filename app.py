1e89cf5db3a47d1c07e0d77b6a8e241e9cc44647
grayzcale <00xima16@gmail.com>
2025-04-12 14:20:22 -0400

    Add Tailwind CSS integration and update navigation links
    - Updated package.json to include Tailwind CSS dependencies.
    - Added output.css generated by Tailwind CSS.
    - Created styles.css to import Tailwind CSS.
    - Updated base.html to change navigation links for Skills and Projects.
    - Implemented dynamic active link highlighting in the navigation.
    - Created new templates for projects.html and skills.html extending base.html.

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html', t="Home")
 
-@app.route('/about')
-def about():
-	return 'About Page'
+@app.route('/skills')
+def skills():
+	return render_template('skills.html', t="Skills")
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html', t="Projects")
 
 # Run the application
 if __name__ == '__main__':
